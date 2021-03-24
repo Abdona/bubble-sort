8e5193136476af1fd9e0c644882779f2ae6a42ef
marwen-brini <brini.marwen@gmail.com>
2021-03-24 11:32:02 +0100

    linter error fix #5

--- index.rb
+++ index.rb
@@ -2,7 +2,7 @@ def bubble_sort(array)
   n = array.length
   loop do
    swapped = false
-    (n-1).times do |i|
+   (n-1).times do |i|
     if array[i]>array[i+1]
       array[i],array[i+1] = array[i+1],array[i]
       swapped=true
