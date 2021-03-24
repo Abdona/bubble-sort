71bc67954d0df7079e7c891359e5be0cb279ddd8
marwen-brini <brini.marwen@gmail.com>
2021-03-24 11:27:47 +0100

    linter error fix #2

--- index.rb
+++ index.rb
@@ -12,21 +12,6 @@ def bubble_sort(array)
 	end
 	array
 end
-
-def bubble_sort(array)
-	n = array.length
-	loop do
-		swapped = false
-		(n-1).times do |i|
-			if array[i]>array[i+1]
-				array[i],array[i+1] = array[i+1],array[i]
-				swapped=true
-			end
-		end
-		break if not swapped
-	end
-	array
-end
 def bubble_sort_by(arr)
   (0..arr.length - 2).each do |i|
     sort_it = yield(arr[i], arr[i + 1])
